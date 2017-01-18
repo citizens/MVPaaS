39bc7d3e72f724df1d887b4d473ecbbdafb666a4
Dylan Montgomery <mail@citizensinspace.com>
2017-01-18 13:39:42 -0500

    created spec helpers for generating pem/jwt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mvpaas"
+
+# Generate a PEM key for testing
+def generate_pem
+  OpenSSL::PKey::RSA.generate(2048)
+end
+
+# Geneate a JWT for testing
+def generate_jwt(pem_key, expiration = nil)
+  payload = {
+    'sub': 123,
+    'username': 'mail@example.com',
+    'exp': expiration || Time.now.to_i + 4 * 3600
+  }
+  JWT.encode(payload, pem_key, 'RS256')
+end
